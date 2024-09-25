0de9dc210dee048e6a65f5409e91e9599af4c306
TimmyLEEY <afolabitimilehin12@gmail.com>
2024-09-25 03:09:07 -0700

    deployment wahala

--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ const cors = require("cors");
 const youtubedl = require("youtube-dl-exec");
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://video-downloaderr.netlify.app/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.post("/download", async (req, res) => {
